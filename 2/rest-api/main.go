f7079c9399e742b5be3101fbcb7caefb3da63c6e
said <sexbythewind@gmail.com>
2026-03-25 23:09:21 +0800

    router group

--- 2/rest-api/main.go
+++ 2/rest-api/main.go
@@ -3,9 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	// store and handler
+	store := NewUserStore()
+	h := NewUserHandler(store)
+	//router
+	r := chi.NewRouter()
+	// middleware
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/users", func(r chi.Router) {
+		r.Get("/", h.ListUsers)
+		r.Post("/", h.CreateUser)
+		r.Get("/{id}", h.GetUser)
+		r.Put("/{id}", h.UpdateUser)
+		r.Delete("/{id}", h.DeleteUser)
+	})
 
 	port := ":8080"
 	log.Printf("Starting server on localhost%s\n", port)
