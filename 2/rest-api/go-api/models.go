11cf0163e83a6ef7d88316c35522981dfd0459f7
said <sexbythewind@gmail.com>
2026-03-28 23:10:50 +0800

    chore: delete other struct to follow code

--- 2/rest-api/go-api/models.go
+++ 2/rest-api/go-api/models.go
@@ -8,19 +8,3 @@ type User struct {
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
 }
-
-type CreateUserRequest struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-type UpdateUserRequest struct {
-	Name  *string `json:"name"`
-	Email *string `json:"email"`
-}
-
-type APIResponse struct {
-	Success bool   `json:"success"`
-	Data    any    `json:"data,omitempty"`
-	Error   string `json:"error,omitempty"`
-}
