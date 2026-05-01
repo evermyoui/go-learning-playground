dd9d349cadd0b69c465144ab070d1ef2342bb66e
said <sexbythewind@gmail.com>
2026-05-01 11:19:42 +0800

    feat: paths for templates

--- django/2/core/core/urls.py
+++ django/2/core/core/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from authentication.views import home, register_page, login_page
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home, name="home"),
+    path('register/', register_page, name="register_page"),
+    path('login/', login_page, name='login_page')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
