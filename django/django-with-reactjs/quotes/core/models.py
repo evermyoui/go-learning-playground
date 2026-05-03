50986bb57c9b21878e939c9c661e2dc6883a969f
said <sexbythewind@gmail.com>
2026-05-03 21:13:16 +0800

    feat: serializers

--- django/django-with-reactjs/quotes/core/models.py
+++ django/django-with-reactjs/quotes/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class React(models.Model):
+    name = models.CharField(max_length=20)
+    detail = models.TextField()
+
+    def __str__(self):
+        return self.name
